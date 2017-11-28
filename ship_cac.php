576f70cadd755d98b01a88566645068c8161cc74
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-28 11:11:52 +0800

    更新配送方式插件

--- ship_cac.php
+++ ship_cac.php
@@ -84,6 +84,5 @@ Ecjia_PluginManager::extend('ship_cac', function() {
 
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_ship_cac', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_ship_cac', 'uninstall'));
-RC_Hook::add_filter('shipping_factory_adapter_instance', array( 'plugin_ship_cac', 'adapter_instance' ), 10, 2);
 
 // end
